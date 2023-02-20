f9a4ef338f754db9cb3bf1f58779b705a345a223
maricormorillo <maricormorillo@gmail.com>
2023-02-20 14:53:06 +1300

    test

--- Test.java
+++ Test.java
@@ -2,7 +2,7 @@ public class Test
 {
 	public static void main(String[] args)
 	{
-	int tableNo=0
+	int tableNo=0;
 	for(int i = 1; i <= 5; i++)
 		System.out.println(tableNo + " * " +
 			i + " = " + tableNo *1);
